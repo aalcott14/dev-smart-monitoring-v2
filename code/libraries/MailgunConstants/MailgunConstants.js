91be2fd88e2f4533262fc74e4cb7087be4e90e07
Adam Alcott <aalcott14@gmail.com>
2018-11-01 15:40:33 -0500

    Asset created by aalcott14

---
+++ code/libraries/MailgunConstants/MailgunConstants.js
@@ -0,0 +1,8 @@
+const MAILGUN_CONFIG = {
+    // ex. 'key-abcdef01234567891234567890'
+    API_KEY : "a2V5LWUxZjE5ZmU4ODlkZjM1MTRhYzdkNmJkZjY5YmE2MTRj",
+    // ex mg.yoursite.com
+    DOMAIN : "bWcuY2xlYXJibGFkZS5jb20=",
+    // ex. origin@yoursite.com
+    ORIGIN_EMAIL : "c21hcnRtb25pdG9yaW5nQGNsZWFyYmxhZGUuY29t"
+};
