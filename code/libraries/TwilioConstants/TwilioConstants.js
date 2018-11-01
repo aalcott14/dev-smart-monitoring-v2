f4936ba0d5a64866a8ebb159b90f7c5a39ffda0e
Adam Alcott <aalcott14@gmail.com>
2018-11-01 15:40:36 -0500

    Asset created by aalcott14

---
+++ code/libraries/TwilioConstants/TwilioConstants.js
@@ -0,0 +1,12 @@
+
+// TODO Create an account on twilio.com, and enter account credentials below
+// ex. USER: "BC218b72987d86855a5adb921370115a20"
+// ex. PASS: "4579ac6ba4fae7b452c03c64aeae40e7"
+// ex. SOURCE_NUMBER: "(+1 512-713-2913)"
+
+const TWILIO_CONFIG = {
+    USER : "QUMyMThiNzI5ODdkODY4NTNjNWFkYjkyMTM3MDExNWEyMA==",
+    PASS : "NDU3OWNhNmJhNGZhZTdiMjMyYzAzYzY0YWVhZTQwZTc=",
+    SOURCE_NUMBER : "KCsxIDUxMi03MTAtMjM4Myk="
+}
+
